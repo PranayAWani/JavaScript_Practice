101e3205111199b5c82926aaf5bd6487edd93e34
PranayAWani <pranaywani04@gmail.com>
2023-12-23 11:24:18 +0530

    Practice

--- P1_Q3,Q4.js
+++ P1_Q3,Q4.js
@@ -1,4 +1,5 @@
 // Create a constant object in javascript. can you change it to hold a number later
+// try to add a new key to the constant object in problem 3. were you are able to do it
 const a=45;
 a=56;
 console.log(a);
---
+++ P1_Q5.js
@@ -0,0 +1,3 @@
+// Write a JS Program to create a word meaning dictonary of 5 words
+
+//--------------------------------------------------
