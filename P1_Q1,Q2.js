87960eafeb9cd75677c8a1b606618aa796a774cc
PranayAWani <pranaywani04@gmail.com>
2023-12-23 11:12:15 +0530

    Practice

---
+++ P1_Q1,Q2.js
@@ -0,0 +1,5 @@
+// create a varialble of type string andtry to add a number to it
+    let a="Pranay";
+    let b= 67;
+    console.log(typeof a , typeof b);
+    console.log(a+b);
---
+++ P2_Q3.js
@@ -0,0 +1,4 @@
+// Create a constant object in javascript. can you change it to hold a number later
+const a=45;
+a=56;
+console.log(a);
